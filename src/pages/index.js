8377d98c0430c5a719f5260b05b8ee6e94aeb288
Edwin Bos <edwinbos1@gmail.com>
2019-02-02 16:33:57 +0100

    Added new logo + new app colors

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { Link } from 'gatsby'
 
 import Layout from '../components/layout/layout'
 import Calculate from '../components/calculate/calculate'
+import Logo from '../components/logo/logo'
 
 import './index.css'
 
@@ -10,7 +11,7 @@ const IndexPage = () => (
   <Layout>
     <div class="page page--index">
       <div class="page__header">
-        <h1 class="page__header-title">Fetch</h1>
+        <h1 class="page__header-title"><Logo/></h1>
       </div>
       <div class="page__content">
         <Calculate/>
